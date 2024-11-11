ce87a713c8739135afa19273ed10efc9a6a3c8de
isabot-web <kryke08mine@gmail.com>
2024-11-10 21:09:58 -0400

    Delete scrit.js

--- scrit.js
+++
@@ -1,9 +0,0 @@
-// Função para navegar para outra página
-function navigateTo(page) {
-    window.location.href = page;
-}
-
-// Função para voltar à página inicial
-function goBack() {
-    window.location.href = 'index.html';
-}
